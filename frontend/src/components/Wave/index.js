ea21d601a0e44122ca1ddbd5d123ff5dbf6d2927
yeshealer <yeshealer@gmail.com>
2022-08-01 10:50:50 +0900

    rename btn

--- frontend/src/components/Wave/index.js
+++ frontend/src/components/Wave/index.js
@@ -42,7 +42,7 @@ const Wave = () => {
                 <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-sky-700 group-hover:bg-sky-500 group-hover:-skew-x-12"></span>
                 <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-sky-600 -rotate-12"></span>
                 <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-sky-400 -rotate-12"></span>
-                <span className="relative">Button Text</span>
+                <span className="relative">Send</span>
             </button>
         </div>
     )

1fe2ec9458098b38510f1a7c78603b42543a7638
yeshealer <yeshealer@gmail.com>
2022-08-01 20:44:14 +0900

    update contract

--- contracts/tiger-wave/contracts/TigerWave.sol
+++ contracts/tiger-wave/contracts/TigerWave.sol
@@ -7,13 +7,58 @@ import "hardhat/console.sol";
 contract TigerWave {
     uint256 totalWaves;
 
-    constructor() {
-        console.log("Hello, I'm writing contract");
+    uint256 private seed;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
     }
 
-    function wave() public {
+    Wave[] waves;
+
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
+        console.log("Hello, I'm writing contract");
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 3 minutes < block.timestamp,
+            "Wait 3m"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("Random # generated: %d", seed);
+
+        if (seed < 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money that the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- contracts/tiger-wave/scripts/run.js
+++ contracts/tiger-wave/scripts/run.js
@@ -1,18 +1,36 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("TigerWave");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address)
-    console.log("Contract deployed by:", owner.address)
+    console.log("Contract addy:", waveContract.address);
 
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
-    let waveTxn = await waveContract.wave();
+    /*
+     * Let's try two waves now
+     */
+    const waveTxn = await waveContract.wave("This is wave #1");
     await waveTxn.wait();
 
-    waveCount = await waveContract.getTotalWaves();
+    const waveTxn2 = await waveContract.wave("This is wave #2");
+    await waveTxn2.wait();
+
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 };
 
 const runMain = async () => {

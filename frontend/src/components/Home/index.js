73ebad7e0b8a98e8c6b5d04bb15bf67d98adabba
yeshealer <yeshealer@gmail.com>
2022-08-29 22:39:46 -0400

    update homepage

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion"
 import { Player } from '@lottiefiles/react-lottie-player';
 
 const Home = () => {
@@ -8,16 +7,7 @@ const Home = () => {
     return (
         <div className="w-fit flex justify-center m-auto mt-36 rotate-45">
             <div className="mr-2">
-                <motion.div
-                    whileHover={{
-                        borderRadius: ['0%', '100%', '0%'],
-                        scale: [1, 1.2, 1.15],
-                        top: '-20px',
-                        right: '20px'
-                    }}
-                    transition={{
-                        duration: 0.5
-                    }}
+                <div
                     className="w-[17rem] h-[17rem] flex relative justify-center items-center bg-slate-500/20 hover:bg-sky-500/30 ease-in duration-200 shadow-xl cursor-pointer mb-2 border-t border-l border-sky-400"
                     onClick={() => navigate('/chat')}
                 >
@@ -28,48 +18,30 @@ const Home = () => {
                         src="https://assets10.lottiefiles.com/packages/lf20_rcuthdnb.json"
                         className="w-56 h-56 -rotate-45"
                     />
-                </motion.div>
-                <motion.div
-                    whileHover={{
-                        borderRadius: ['0%', '100%', '0%'],
-                        scale: [1, 1.25, 1.2],
-                        bottom: '-26px',
-                        right: '26px'
-                    }}
-                    className="w-[17rem] h-[17rem] flex relative justify-center items-center bg-slate-500/20 hover:bg-sky-500/30 ease-in duration-200 shadow-xl cursor-pointer mb-2 border-l border-b border-sky-400"
+                </div>
+                <div
+                    className="w-[17rem] h-[17rem] flex relative justify-center items-center bg-slate-500/20 hover:bg-sky-500/30 ease-in duration-200 shadow-xl cursor-pointer border-l border-b border-sky-400"
                 >
                     <div className="w-44 h-44 flex justify-center items-center bg-slate-500/20 rounded-full -rotate-45">
                         <h1 className="text-3xl font-black">Wave</h1>
                     </div>
-                </motion.div>
+                </div>
             </div>
             <div>
-                <motion.div
-                    whileHover={{
-                        borderRadius: ['0%', '100%', '0%'],
-                        scale: [1, 1.25, 1.2],
-                        top: '-26px',
-                        left: '26px'
-                    }}
+                <div
                     className="w-[17rem] h-[17rem] flex relative justify-center items-center bg-slate-500/20 hover:bg-sky-500/30 ease-in duration-200 shadow-xl cursor-pointer mb-2 border-t border-r border-sky-400"
                 >
                     <div className="w-44 h-44 flex justify-center items-center bg-slate-500/20 rounded-full -rotate-45">
                         <h1 className="text-3xl font-black">Wave</h1>
                     </div>
-                </motion.div>
-                <motion.div
-                    whileHover={{
-                        borderRadius: ['0%', '100%', '0%'],
-                        scale: [1, 1.25, 1.2],
-                        bottom: '-26px',
-                        left: '26px'
-                    }}
+                </div>
+                <div
                     className="w-[17rem] h-[17rem] flex relative justify-center items-center bg-slate-500/20 hover:bg-sky-500/30 ease-in duration-200 shadow-xl cursor-pointer mb-2 border-r border-b border-sky-400"
                 >
                     <div className="w-44 h-44 flex justify-center items-center bg-slate-500/20 rounded-full -rotate-45">
                         <h1 className="text-3xl font-black">Wave</h1>
                     </div>
-                </motion.div>
+                </div>
             </div>
         </div>
     )
